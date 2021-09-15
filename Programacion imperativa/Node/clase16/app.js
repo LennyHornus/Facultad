f73c6c7b33ed850b49d59fd83cd3cc529e7d1b9d
LennyHornus <lennyrh25@gmail.com>
2021-09-14 21:52:02 -0300

    fin 14/09

---
+++ Programacion imperativa/Node/clase16/app.js
@@ -0,0 +1,70 @@
+const v = "\x1b[32m%s\x1b[0m";
+const o = "*".repeat(80) + "\n";
+const oo = "*".repeat(25);
+// Te proveemos la siguiente plantilla que tiene tre partes:
+// - Array de objetos que está colapsado aquí debajo.
+// - Desarrollo de las consignas, donde escribirás el código que responda a los enunciados
+// - Ejecución de las consignas, donde ejecutarás los métodos correspondientes mostrando por consola sus resultados
+const nombre = "Lenny Ramos Hornus";
+const tema = "Tema 1";
+
+const inmobiliaria = require('./objApp')
+
+/******************************/
+/* Ejecución de las consignas */
+/******************************/
+console.table([{ alumno: nombre, tema: tema }]);
+
+console.log(v, "\n" + oo + "   B. listarDepartamentos");
+inmobiliaria.listarDepartamentos(inmobiliaria.departamentos);
+console.log(o);
+
+console.log(v, oo + "   C. departamentosDisponibles");
+const disponibles = inmobiliaria.departamentosDisponibles();
+inmobiliaria.listarDepartamentos(disponibles);
+console.log(o);
+
+console.log(v, oo + " D. buscarPorId");
+const unId = inmobiliaria.buscarPorId(1);
+inmobiliaria.listarDepartamentos([unId]);
+console.log(o);
+
+console.log(v, oo + "  E. buscarPorPrecio");
+const unPrecio = inmobiliaria.buscarPorPrecio(1000);
+inmobiliaria.listarDepartamentos(unPrecio);
+console.log(o);
+
+console.log(v, oo + " F. precioConImpuesto");
+const unImpuesto = inmobiliaria.precioConImpuesto(10);
+inmobiliaria.listarDepartamentos(unImpuesto);
+console.log(o);
+
+console.log(v, oo + " G. simplificarPropietarios");
+const propietarios = inmobiliaria.simplificarPropietarios();
+inmobiliaria.listarDepartamentos(propietarios);
+console.log(o);
+
+console.log(v, oo + " H. Alquilar un departamento");
+const vacas2021 = inmobiliaria.alquilar(1);
+inmobiliaria.listarDepartamentos([vacas2021]); ////// OJO CON QUE LA FUNCION SOLO IMPRIME ARRAYS
+console.log(o);
+
+console.log(v, oo + " I. Aceptan mascotas?");
+const mascotas = inmobiliaria.filtrarPetFriendly();
+inmobiliaria.listarDepartamentos(mascotas);
+console.log(o);
+
+console.log(v, oo + " J. Ofertas o algo asi");
+const Ofertas = inmobiliaria.rebajasPorNoAlquiler();
+inmobiliaria.listarDepartamentos(Ofertas);
+console.log(o);
+
+console.log(v, oo + " K. Buscar por propietarios");
+const prop = inmobiliaria.buscarPorPropietarios('Juan');
+inmobiliaria.listarDepartamentos(prop);
+console.log(o);
+
+console.log(v, oo + " INTENTO 2 PRECIO IMPUESTO ");
+const impuesto = inmobiliaria.precioConImpostor(10);
+inmobiliaria.listarDepartamentos(impuesto);
+console.log(o);
---
+++ Programacion imperativa/Node/clase16/objApp.js
@@ -0,0 +1,132 @@
+/*******************************/
+/* Desarrollo de las consignas */
+/*******************************/
+
+const inmobiliaria = {
+    // A
+    departamentos : require('./datos'),
+    // B
+    listarDepartamentos: function (array) {
+      for (let i = 0; i < array.length; i++) {
+        const dpto = array[i];
+        console.log(
+          `id: ${dpto.id}, precio $${dpto.precioAlquiler}, está ${
+            dpto.disponible ? "Disponible" : "Alquilado"
+          }, ${dpto.cantidadHabitacion} ambientes, máximo ${
+            dpto.cantidadPersonas
+          } personas, ${
+            dpto.aceptaMascotas ? "Acepta mascotas" : "No acepta mascotas"
+          }, ${dpto.propietarios}` + "\n"
+        );
+      }
+    },
+    // C
+    departamentosDisponibles: function () {
+      const habilitados = [];
+      for (let i = 0; i < this.departamentos.length; i++) {
+        const dpto = this.departamentos[i];
+        if (dpto.disponible) {
+          habilitados.push(dpto);
+        }
+      }
+      return habilitados;
+    },
+    // D
+    buscarPorId: function (id) {
+      for (let i = 0; i < this.departamentos.length; i++) {
+        const dpto = this.departamentos[i];
+        if (dpto.id === id) {
+          return dpto;
+        }
+      }
+    },
+    // E
+    buscarPorPrecio: function (precio) {
+      const filtrados = [];
+      const disponibles = this.departamentosDisponibles();
+      for (let i = 0; i < disponibles.length; i++) {
+        const dpto = disponibles[i];
+        if (dpto.precioAlquiler <= precio) {
+          filtrados.push(dpto);
+        }
+      }
+      return filtrados;
+    },
+    // F
+    precioConImpuesto: function (porcentaje) {
+      const factor = 1 + (1 * porcentaje) / 100;
+      const actualizados = [];
+      for (let i = 0; i < this.departamentos.length; i++) {
+        const dpto = this.departamentos[i];
+        // Como lo habia hecho, pero me quedaba mejor como lo hizo el profe con el toFixed
+        //   dpto.precioAlquiler *= factor
+        dpto.precioAlquiler = (dpto.precioAlquiler * factor).toFixed(2);
+        actualizados.push(dpto);
+      }
+      return actualizados;
+    },
+  
+    // OPCION ALTERNATIVA PARA IMPUESTO
+    precioConImpostor : function (porcentaje) {
+      const actualizados = [];
+      for (let i = 0; i < this.departamentos.length; i++) {
+        const dpto = this.departamentos[i];
+        dpto.precioAlquiler = (dpto.precioAlquiler * porcentaje).toFixed(2);
+        actualizados.push(dpto); 
+      }
+      return actualizados;
+    },
+    // G
+    simplificarPropietarios: function () {
+      for (let i = 0; i < this.departamentos.length; i++) {
+        const losPropietarios = this.departamentos[i].propietarios;
+        const desde = losPropietarios.indexOf(":");
+        const soloPropietarios = losPropietarios.slice(desde);
+        this.departamentos[i].propietarios = "Prop." + soloPropietarios;
+      }
+      return this.departamentos;
+    },
+    // H
+    alquilar: function (id) {
+      const porAlquilar = inmobiliaria.buscarPorId(id);
+      if (porAlquilar.disponible) {
+        porAlquilar.disponible = false;
+      }
+      return porAlquilar;
+    },
+    // I
+    filtrarPetFriendly: function () {
+      const dptoPetFriendly = [];
+      for (let i = 0; i < this.departamentos.length; i++) {
+        const dpto = this.departamentos[i];
+        if (dpto.aceptaMascotas === true) {
+          dptoPetFriendly.push(dpto);
+        }
+      }
+      return dptoPetFriendly;
+    },
+    // J
+    rebajasPorNoAlquiler: function () {
+      const rebajas = [];
+      for (let i = 0; i < this.departamentos.length; i++) {
+        const dpto = this.departamentos[i];
+        if (dpto.disponible) {
+          dpto.precioAlquiler = (dpto.precioAlquiler * 0.9).toFixed(2);
+          rebajas.push(dpto);
+        }
+      }
+      return rebajas;
+    },
+    buscarPorPropietarios: function (propietario) {
+      const dptoPropietarios = [];
+      for (let i = 0; i < this.departamentos.length; i++) {
+        const dpto = this.departamentos[i];
+        if (dpto.propietarios.includes(propietario)) {
+          dptoPropietarios.push(dpto);
+        }
+      }
+      return dptoPropietarios;
+    },
+};
+
+module.exports = inmobiliaria;
