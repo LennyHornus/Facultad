199205ce7b7bb8239f78e1c73c858602ee110156
LennyHornus <lennyrh25@gmail.com>
2021-11-28 18:45:59 -0300

    puedo eliminar de 1 las tareas jaja

--- bimestre2/front2/ToDo inicial/scripts/signup.js
+++ bimestre2/front2/ToDo inicial/scripts/signup.js
@@ -22,6 +22,13 @@ formularioSignUp.addEventListener("submit", function(event) {
     event.preventDefault();
     let errores = [];
 
+    function cargarMensajeVacio(inputName) {
+        errores.push({
+            input: inputName,
+            mensaje: ''
+        })
+    }
+
     // Errores del nombre
     let inputNombreValue = inputNombre.value.trim();
     
@@ -30,7 +37,9 @@ formularioSignUp.addEventListener("submit", function(event) {
             input: "nombre",
             mensaje: "Este campo es obligatorio"
         })
-    };
+    } else {
+        cargarMensajeVacio('nombre')
+    }
 
     // Errores del apellido
     let inputApellidoValue = inputApellido.value.trim();
@@ -40,7 +49,10 @@ formularioSignUp.addEventListener("submit", function(event) {
             input: "apellido",
             mensaje: "Este campo es obligatorio"
         })
-    };
+    }
+    else {
+        cargarMensajeVacio('apellido')
+    }
 
     // Errores del email
     if(inputEmail.value.trim() === "") {
@@ -48,35 +60,33 @@ formularioSignUp.addEventListener("submit", function(event) {
             input: "emailVacio",
             mensaje: "Este campo es obligatorio"
         })
-    };
-
-    if(!inputEmail.value.includes('@') && inputEmail.value.length > 1) {
+    }else if (!inputEmail.value.match(/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)$/)) {
         errores.push({
             input: "emailNoValido",
             mensaje: "Ingrese un email con formato valido"
         })
+    } else {
+        cargarMensajeVacio('emailNoValido')
     }
     
     // Errores de la password
-    if(inputPass.value === "") {
+    if(inputPass.value === null) {
         errores.push({
             input: "pass",
             mensaje: "Este campo es obligatorio"
         })
-    }
-
-    if (inputPass.value.includes(' ')) { // Con el includes(' '), corroboro que la contraseña no contenga espacios
-        errores.push({                   // Si los contiene, pusheo al array de errores un mensaje que indique que la contraseña no debe llevar espacios
+    } else if (inputPass.value.includes(' ')) {         // Con el includes(' '), corroboro que la contraseña no contenga espacios
+        errores.push({                                  // Si los contiene, pusheo al array de errores un mensaje que indique que la contraseña no debe llevar espacios
             input: 'passContainsSB',
             mensaje: 'La contraseña no puede llevar espacios'
         })
-    }
-
-    if (inputPass.value.length < 7 && inputPass.value.length > 0) { // Con el .length corroboro que la longitud de la contraseña
-        errores.push({                                          // sea menor a 7 caracteres y mayor a 0, pusheo al array de errores el mensaje
+    } else if (inputPass.value.length < 7 && inputPass.value.length > 0) {      // Con el .length corroboro que la longitud de la contraseña
+        errores.push({                                                          // sea menor a 7 caracteres y mayor a 0, pusheo al array de errores el mensaje
             input: 'passShort',
             mensaje: 'La contraseña debe ser mayor a 7 caracteres'
         })
+    } else {
+        cargarMensajeVacio('pass')
     }
 
     // Errores del Repetir contraseña
@@ -86,7 +96,9 @@ formularioSignUp.addEventListener("submit", function(event) {
             mensaje: "Las contraseñas no coinciden"
         })
     }
-    
+    else {
+        cargarMensajeVacio('passRepe')
+    }
 
     // Aca cargo los errores en el DOM
     if(errores.length !== 0) {             // Si el array errores no es 0, es decir que contiene errores, ejecuto un forEach de cada elemento
