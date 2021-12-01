745074af9c72ce5691ae5c11623101456b7383ff
LennyHornus <lennyrh25@gmail.com>
2021-12-01 20:00:31 -0300

    fub 1/12

--- bimestre2/front2/ToDo inicial/scripts/index.js
+++ bimestre2/front2/ToDo inicial/scripts/index.js
@@ -1,27 +1,30 @@
 // Traigo desde el DOM los elementos que necesito
-const formularioInicio = document.querySelector('form');
+let formularioInicio = document.querySelector('form');
 
-const inputEmail = document.querySelector('#inputEmail');
-const errorEmail = document.querySelector('#errorEmail');
+let inputEmail = document.querySelector('#inputEmail');
+let errorEmail = document.querySelector('#errorEmail');
 
-const inputPass = document.querySelector('#inputPassword');
-const errorPass = document.querySelector('#errorPass');
+let inputPass = document.querySelector('#inputPassword');
+let errorPass = document.querySelector('#errorPass');
 
-const btnSubmit = document.querySelector('button');
+let btnSubmit = document.querySelector('button');
 
 formularioInicio.addEventListener('submit', (e)=>{
     e.preventDefault();     // Freno el envio del formulario
     let errores = [];
 
+    errorEmail.innerText = '';
+    errorPass.innerText = '';
+
     // Errores del email
-    if(inputEmail.value.trim() === "") {
+    if(inputEmail.value.length === 0) {
         errores.push({
             input: "emailVacio",
             mensaje: "Ingrese un email"
         })
     };
 
-    if(!inputEmail.value.includes('@') && inputEmail.value.length > 1) {
+    if(!inputEmail.value.match(/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)$/)) {
         errores.push({
             input: "emailNoValido",
             mensaje: "Ingrese un email con formato valido"
@@ -29,7 +32,7 @@ formularioInicio.addEventListener('submit', (e)=>{
     }
     
     // Errores de la password
-    if(inputPass.value === "") {
+    if(!inputPass.value) {
         errores.push({
             input: "pass",
             mensaje: "Ingrese una contraseña"
@@ -51,6 +54,26 @@ formularioInicio.addEventListener('submit', (e)=>{
             }
         })
     } else {
+        const data = {
+            email: inputEmail.value,
+            password: inputPass.value
+        }
+        
+        const settings = {
+            "method": "POST",
+            "body": JSON.stringify(data)
+        }
+        
+        fetch("https://ctd-fe2-todo.herokuapp.com/v1/users/login", settings)
+            .then((response)=>{
+                return response.json();
+            })
+            .then((info)=>{
+                console.log(info);
+            })
+            .catch((error)=>{
+                console.error("Error! " + error);
+            })
         formularioSignUp.submit(); // Si no sucede ninguno de los casos anteriores, envio el formulario
     }
 });
