91fefc5cfaa8261ce0a8501238f0c37fbeed7875
LennyHornus <lennyrh25@gmail.com>
2021-12-03 20:05:10 -0300

    fin 3/12

--- bimestre2/front2/ToDo inicial/scripts/task.js
+++ bimestre2/front2/ToDo inicial/scripts/task.js
@@ -3,9 +3,11 @@ if (!sessionStorage.token && !localStorage.token) {
     // Si no existe el token en el localStorage ni en el sessionStorage lo mando de nuevo a iniciar sesion
 }
 
+
 // -------------------------------------------------------------- //
 // Cargo la informacion del usuario 
 // -------------------------------------------------------------- //
+
 const userName = document.querySelector('.user-info p');
 
 // Traigo el token primero de localStorage si existe, si no existe traigo el de sessionStorage
@@ -27,9 +29,11 @@ fetch(urlGetMe, settingsInfoUser)
         return datosUser;
 })
 
+
 // -------------------------------------------------------------- //
 // Agrego funcionalidad al boton de cerrar sesion 
 // -------------------------------------------------------------- //
+
 const btnCerrarSesion = document.querySelector('#closeApp');
 btnCerrarSesion.addEventListener('click', () => {
     let confirmacion = confirm("¿Desea cerrar sesion?")
@@ -43,9 +47,11 @@ btnCerrarSesion.addEventListener('click', () => {
 
 const urlTareas = 'https://ctd-fe2-todo.herokuapp.com/v1/tasks';
 
+
 // -------------------------------------------------------------- //
 // Creo la funcion para consultar las tareas con un GET de la API 
 // -------------------------------------------------------------- //
+
 function consultarTareas() {
     const settings = {
         method: 'GET',
@@ -57,89 +63,97 @@ function consultarTareas() {
     fetch(urlTareas, settings)
       .then(response => response.json())
       .then(tareas => {
-        console.log(tareas)
-
-        // Selecciono el esqueleto con las tareas predeterminadas y las borro
-        const skeleton = document.querySelector('#skeleton');   
-        if (skeleton) {
-          skeleton.remove();
-        }
-
         renderizarTareas(tareas);
+        const arrayBotones = document.querySelectorAll('.not-done');
+        terminarTarea(arrayBotones, tareas);
         // botonBorrarTarea();
       })
       .catch(error => console.log(error));
 }
 consultarTareas();  // La hago correr una vez para traer tareas si ya existian
 
+
 // -------------------------------------------------------------- //
 // Creo la funcion para renderizar las tareas
 // -------------------------------------------------------------- //
+
 function renderizarTareas(listadoTareas) {
-    const contenedorTerminadas = document.querySelector('.tareas-terminadas');
-    contenedorTerminadas.innerHTML = '';
-    const contenedorPendientes = document.querySelector('.tareas-pendientes');
-    contenedorPendientes.innerHTML = '';
+  // Selecciono el esqueleto con las tareas predeterminadas y las borro
+  const skeleton = document.querySelector('#skeleton');   
+  if (skeleton) {
+    skeleton.remove();
+  }
 
-    listadoTareas.forEach(tarea => {
-        let fecha = new Date(tarea.createdAt); // Casteo createdAt como una Date para poder usar sus metodos
+  const contenedorTerminadas = document.querySelector('.tareas-terminadas');
+  contenedorTerminadas.innerHTML = '';
+  const contenedorPendientes = document.querySelector('.tareas-pendientes');
+  contenedorPendientes.innerHTML = '';
 
-        if (!tarea.completed) {
-            contenedorPendientes.innerHTML += `
-                        <li class="tarea">
-                            <div class="not-done change" id="${tarea.id}"></div>
-                            <div class="descripcion">
-                                <p class="nombre">${tarea.description}</p>
-                                <p class="timestamp"><i class="far fa-calendar-alt"></i> ${fecha.toLocaleDateString()} <i class="far fa-clock"></i> ${fecha.getHours()}:${fecha.getMinutes()}</p>
-                            </div>
-                        </li>`
-        }
-        else {
-            contenedorTerminadas.innerHTML += `
-                        <li class="tarea">
-                            <div class="done"></div>
-                            <div class="descripcion">
-                            <div>
-                                <button><i id="${tarea.id}" class="fas fa-undo-alt change"></i></button>
-                                <button><i id="${tarea.id}" class="far fa-trash-alt"></i></button>
-                            </div>
-                            <p class="nombre">${tarea.description}</p>
+  listadoTareas.forEach(tarea => {
+      let fecha = new Date(tarea.createdAt); // Casteo createdAt como una Date para poder usar sus metodos
+
+      if (!tarea.completed) {
+          contenedorPendientes.innerHTML += `
+                      <li class="tarea">
+                          <div class="not-done change" id="${tarea.id}"></div>
+                          <div class="descripcion">
+                              <p class="nombre">${tarea.description}</p>
+                              <p class="timestamp"><i class="far fa-calendar-alt"></i> ${fecha.toLocaleDateString()} <i class="far fa-clock"></i> ${fecha.getHours()}:${fecha.getMinutes()}</p>
+                          </div>
+                      </li>`
+      }
+      else {
+          contenedorTerminadas.innerHTML += `
+                      <li class="tarea">
+                          <div class="done"></div>
+                          <div class="descripcion">
+                           <div>
+                               <button><i id="${tarea.id}" class="fas fa-undo-alt change"></i></button>
+                               <button><i id="${tarea.id}" class="far fa-trash-alt"></i></button>
+                           </div>
+                           <p class="nombre">${tarea.description}</p>
                             <p class="timestamp"><i class="far fa-calendar-alt"></i> ${fecha.toLocaleDateString()} <i class="far fa-clock"></i> ${fecha.getHours()}:${fecha.getMinutes()}</p>
-                            </div>
-                        </li>`
-        }
+                           </div>
+                       </li>`
+      }
     })
 };
+
 
 // -------------------------------------------------------------- //
 // Creo la funcion para subir las tareas por POST a la API
 // -------------------------------------------------------------- //
+
 const btnAdd = document.querySelector('#addToDo');
 const nuevaTarea = document.querySelector('#nuevaTarea');
 
 function subirTarea() {
-    // mando a la api un post de la tarea 
-    const datosTareaPost = {
-        method: 'POST',
-        headers: {
-            authorization: token
-        },
-        body: {
-            description: nuevaTarea.value,
-            completed: false
-        }
-    };
-    console.log("Subiendo tarea");
-    fetch(urlTareas, datosTareaPost)
-      .then(response => response.json())
-      .then(datosTareaNueva => {
-        console.log(datosTareaNueva)
-        if (datosTareaNueva) {
-            consultarTareas();
-            // botonBorrarTarea();
-        }
-      })
-      .catch(error => console.log(error)); 
+  // mando a la api un post de la tarea
+  const bodyNota = {
+      description: nuevaTarea.value,
+      completed: false
+  }
+  const datosTareaPost = {
+      "method": "POST",
+      "headers": {
+          "authorization": `${token}`,
+          "content-type": "application/json"
+      },
+      "body": JSON.stringify(bodyNota)
+  };
+  
+  console.log("Subiendo tarea");
+
+  fetch(urlTareas, datosTareaPost)
+    .then(response => response.json())
+    .then(datosTareaNueva => {
+      console.log(datosTareaNueva)
+      if (datosTareaNueva) {
+          consultarTareas();
+          nuevaTarea.value = '';
+      }
+    })
+    .catch(error => console.log(error)); 
 }
 
 btnAdd.addEventListener('click', (e) => {
@@ -149,13 +163,38 @@ btnAdd.addEventListener('click', (e) => {
 
 
 // -------------------------------------------------------------- //
-// Creo la funcion para terminar las tareas pendientes, con un UPDATE a la API
+// Creo la funcion para terminar las tareas pendientes, con un PUT a la API
 // -------------------------------------------------------------- //
 
-// function botonTerminarTarea() {
-//     aca le doy funcion al boton de terminar tarea, mando un UPDATE del estado
-//     de la tarea a la api
-// }
+function terminarTarea(arrayBotones, arrayTareas) { 
+  arrayBotones.forEach((boton, i) => {
+    boton.addEventListener('click', (e)=> {
+      e.preventDefault();
+      
+      const datosTareaPut = {
+        "method": "PUT",
+        "headers": {
+          "authorization": token,
+          "Content-type": "application/json; charset=UTF-8"
+        },
+        "body": JSON.stringify({
+          "completed": true
+        })
+      };
+
+      console.log("Terminando tarea");
+
+      fetch(`${urlTareas}/${arrayTareas[i].id}`, datosTareaPut)
+        .then(response => response.json())
+        .then(datosPut => {
+          if (datosPut) {
+            consultarTareas();
+          }
+        })
+        .catch(error => console.log(error)); 
+    })
+  })
+}
 
 // -------------------------------------------------------------- //
 // Creo la funcion para borrar tareas con un DELETE a la API
